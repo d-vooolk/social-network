edf0bc10793def8526d6460c4c508353c6cb6757
d-vooolk <nestwedman@gmail.com>
2021-11-11 03:15:46 +0300

    add hashrouter

---
+++ precache-manifest.b45200a1f4e36b9e7ab136befcf6672c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a8785f2f0c0ad0099835951921759b",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "3daadb6b6422e7f2490c",
+    "url": "/social-network/static/css/3.7b1bae76.chunk.css"
+  },
+  {
+    "revision": "9c39e283c56f964653ac",
+    "url": "/social-network/static/css/4.d166f73f.chunk.css"
+  },
+  {
+    "revision": "4798160f439d7c24edef",
+    "url": "/social-network/static/css/main.e5de328d.chunk.css"
+  },
+  {
+    "revision": "a9a9d8ea4eb115686926",
+    "url": "/social-network/static/js/2.fdc21cd7.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/social-network/static/js/2.fdc21cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3daadb6b6422e7f2490c",
+    "url": "/social-network/static/js/3.42bdf381.chunk.js"
+  },
+  {
+    "revision": "9c39e283c56f964653ac",
+    "url": "/social-network/static/js/4.837ff75b.chunk.js"
+  },
+  {
+    "revision": "7f0eec8069a192085ec8",
+    "url": "/social-network/static/js/5.ccba1a9a.chunk.js"
+  },
+  {
+    "revision": "4798160f439d7c24edef",
+    "url": "/social-network/static/js/main.0e7f22f7.chunk.js"
+  },
+  {
+    "revision": "87054633d713e3f65f24",
+    "url": "/social-network/static/js/runtime-main.9b094684.js"
+  },
+  {
+    "revision": "6d48f5cbf9b708fc74422d1519ae908c",
+    "url": "/social-network/static/media/header-avatar.6d48f5cb.svg"
+  },
+  {
+    "revision": "6d48f5cbf9b708fc74422d1519ae908c",
+    "url": "/social-network/static/media/header-image.6d48f5cb.svg"
+  },
+  {
+    "revision": "1a53c75355ca07d639467977d34a27d7",
+    "url": "/social-network/static/media/spinner.1a53c753.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-network/precache-manifest.9f531c67094d3b4b25425f9fc6168408.js"
+  "/social-network/precache-manifest.b45200a1f4e36b9e7ab136befcf6672c.js"
 );
 
 self.addEventListener('message', (event) => {
